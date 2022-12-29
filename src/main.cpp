509b102e190cc18ab66d8c1e836d582994e6e875
Daniel <danielcoco96@gmail.com>
2022-12-29 23:04:55 +0100

    Mapped SNES controller to BLE Gamepad

--- src/main.cpp
+++ src/main.cpp
@@ -14,84 +14,96 @@ int previousButton1State = HIGH;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Hello World");
 
-    //initialize shared pins
+  //initialize shared pins
   controllers.init(LATCH_PIN, CLOCK_PIN); 
   //activate first controller ans set the type to SNES
   controllers.setController(0, GameControllers::SNES, DATA_PIN_0);
 
   bleGamepad.begin();
-
 }
+
+
 
 void loop() {
   controllers.poll(); //read all controllers at once
-  delay(50);  //read controller just ~20times a second
+
   if (bleGamepad.isConnected())
     {
-        if(controllers.down(0, GameControllers::A))
-        {
-            bleGamepad.press(BUTTON_1);
-        }
-        else
-        {
-            bleGamepad.release(BUTTON_1);
-        }
+      // A/B/X/Y
+      if (controllers.down(0, GameControllers::A)){
+        bleGamepad.press(BUTTON_1);
+      }
+      else{
+          bleGamepad.release(BUTTON_1);
+      }
 
-        if(controllers.down(0, GameControllers::B))
-        {
-            bleGamepad.press(BUTTON_2);
-        }
-        else
-        {
-            bleGamepad.release(BUTTON_2);
-        }
+      if(controllers.down(0, GameControllers::B)){
+          bleGamepad.press(BUTTON_2);
+      }
+      else{
+          bleGamepad.release(BUTTON_2);
+      }
 
-        if(controllers.down(0, GameControllers::UP))
-        {
-            bleGamepad.setHat1(DPAD_UP);
-        }
-        else
-        {
-            bleGamepad.setHat1(HAT_CENTERED);
-        }
+      if(controllers.down(0, GameControllers::X)){
+          bleGamepad.press(BUTTON_4);
+      }
+      else{
+          bleGamepad.release(BUTTON_4);
+      }
+
+      if(controllers.down(0, GameControllers::Y)){
+          bleGamepad.press(BUTTON_5);
+      }
+      else{
+          bleGamepad.release(BUTTON_5);
+      }
+
+      // L1/R1
+      if(controllers.down(0, GameControllers::L)){
+          bleGamepad.press(BUTTON_7);
+      }
+      else{
+          bleGamepad.release(BUTTON_7);
+      }
+      if(controllers.down(0, GameControllers::R)){
+          bleGamepad.press(BUTTON_8);
+      }
+      else{
+          bleGamepad.release(BUTTON_8);
+      }
+
+      // Start/Select
+      if(controllers.down(0, GameControllers::START)){
+          bleGamepad.press(BUTTON_12);
+      }
+      else{
+          bleGamepad.release(BUTTON_12);
+      }
+      if(controllers.down(0, GameControllers::SELECT)){
+          bleGamepad.press(BUTTON_11);
+      }
+      else{
+          bleGamepad.release(BUTTON_11);
+      }
+
+      // DPAD
+      if(controllers.down(0, GameControllers::UP)){
+        bleGamepad.setHat1(DPAD_UP);
+      }
+      else if (controllers.down(0, GameControllers::DOWN)){        
+        bleGamepad.setHat1(DPAD_DOWN);
+      }
+      else if (controllers.down(0, GameControllers::LEFT)){
+        bleGamepad.setHat1(DPAD_LEFT);
+      }      
+      else if (controllers.down(0, GameControllers::RIGHT)){
+        bleGamepad.setHat1(DPAD_RIGHT);
+      }
+      else{
+        bleGamepad.setHat1(DPAD_CENTERED);
+      }
+
 
     }
-
-
-  
-
-  
-
-  /*
-  if(controllers.down(0, GameControllers::START))  //check if Start was pressed since last loop
-    Serial.println("Start was pressed.");
-  if(controllers.down(0, GameControllers::SELECT))  //check if Start was pressed since last loop
-    Serial.println("Select was pressed.");
-
-  //if(controllers.down(0, GameControllers::A))  //if A button is hold down repeat after 20 loops
-    //Serial.println("A");
-  //if(controllers.down(0, GameControllers::B))  //if A button is hold down repeat after 20 loops
-    //Serial.println("B");
-  if(controllers.down(0, GameControllers::X))  //if A button is hold down repeat after 20 loops
-    Serial.println("X");
-  if(controllers.down(0, GameControllers::Y))  //if A button is hold down repeat after 20 loops
-    Serial.println("Y");
-
-  if(controllers.down(0, GameControllers::L))  //if A button is hold down repeat after 20 loops
-    Serial.println("L");
-    if(controllers.down(0, GameControllers::R))  //if A button is hold down repeat after 20 loops
-    Serial.println("R");
-
-  if(controllers.down(0, GameControllers::UP))  //check if B button it's currently pressed down
-    Serial.println("UP");
-   if(controllers.down(0, GameControllers::DOWN))  //check if B button it's currently pressed down
-    Serial.println("DOWN");
-   if(controllers.down(0, GameControllers::LEFT))  //check if B button it's currently pressed down
-    Serial.println("LEFT");
-   if(controllers.down(0, GameControllers::RIGHT))  //check if B button it's currently pressed down
-    Serial.println("RIGHT");
-  delay(50);  //read controller just ~20times a second
-  */
 }
